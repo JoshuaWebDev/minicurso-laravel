ce03b91114228787f5c876f9078aa1e324f86f13
JoshuaWebDev <josue.barros1986@gmail.com>
2022-11-10 14:57:59 -0300

    layout principal definido

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,44 @@
-@dump($usuarios)
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Mini Sigaa</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+    <div class="main">
+        <div class="top">
+            <div class="top-bar">
+                <h1>UNIFAP - SIGAA - Sistema Integrado de Gestão de Atividades Acadêmicas</h1>
+                <div class="sair">
+                    <a href="#">SAIR</a>
+                </div>
+            </div>
+            <div class="top-middle">
+                <h1>Alunos Cadastrados no Sistema</h1>
+                <h2>DEPARTAMENTO DE CIÊNCIAS EXATAS E TECNOLÓGICAS - DCET(11.02.25.10)</h2>
+            </div>
+        </div>
+        <div class="top-nav">
+            <a class="active" href="#home"><i class="material-icons">school</i> Ensino</a>
+            <a href="#pesquisa"><i class="material-icons">science</i> Pesquisa</a>
+            <a href="#extensão"><i class="material-icons">extension</i> Extensão</a>
+            <a href="#biblioteca"><i class="material-icons">local_library</i> Biblioteca</a>
+            <a href="#bolsas"><i class="material-icons">work</i> Bolsas</a>
+            <a href="#ava"><i class="material-icons">preview</i> Ambientes Virtuais</a>
+            <a href="#outros"><i class="material-icons">link</i> Outros</a>
+        </div>
+        <div class="content">
+            <p>Conteúdo Principal</p>
+        </div>
+        <div class="rodape">
+            <footer>
+                SIGAA | Núcleo de Tecnologia da Informação (NTI-UNIFAP) - (96)3312-1733|Copyright &copy; 2006-2022 - UNIFAP
+            </footer>
+        </div>
+    </div>
+</body>
+</html>
