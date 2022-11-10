5f2e86aa5f272ab9dc68ef69c7581f7ed3c43fe6
JoshuaWebDev <josue.barros1986@gmail.com>
2022-11-10 10:16:18 -0300

    entendendo controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/teste', [UsuarioController::class, 'listarUsuarios']);
+Route::get('/usuarios', [UsuarioController::class, 'listarUsuarios']);
